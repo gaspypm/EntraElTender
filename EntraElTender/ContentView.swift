7623326c4a16be9f121104fed34b4febef2f82c3
Gaspar Pérez Mazás <gaspypm@gmail.com>
2024-03-11 18:03:38 -0300

    Initial Commit

---
+++ EntraElTender/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EntraElTender
+//
+//  Created by Gaspar Perez Mazas on 11/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EntraElTender/EntraElTenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  EntraElTenderApp.swift
+//  EntraElTender
+//
+//  Created by Gaspar Perez Mazas on 11/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct EntraElTenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
